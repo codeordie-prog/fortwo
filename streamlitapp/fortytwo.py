b9ca7b41697d4ed0339630ae2ceb460d26e22fe5
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-07-03 13:33:29 +0300

    tree sitter

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -615,8 +615,8 @@ try:
                 qa = create_retrieval_chain(retriever_chain, document_chain)
 
                 return qa
-        except Exception as e :
-             st.write("an error occured inside the github repo function, its related to parsing of languages that require Tree sitter.",e)
+        except Exception:
+             st.write("an error occured inside the github repo function, its related to parsing of languages that require Tree sitter.")
 
 
        
