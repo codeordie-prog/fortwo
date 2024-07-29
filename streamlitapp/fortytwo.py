79872f452d56c4d2bd4b85ee3e6116f1bf2a1a02
Kelvin Ndeti <136579757+codeordie-prog@users.noreply.github.com>
2024-07-29 09:54:08 +0300

    Update fortytwo.py

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -90,8 +90,8 @@ try:
     st.sidebar.title("chatwith42")
     st.sidebar.subheader("About")
     st.sidebar.info("""
-        42 named after the Hitch Hiker's Guide to the Galaxy answer to the ultimate question of life,
-        is a knowledge discovery engine that utilizes Retrieval Augmented Generation to query uploaded documents in the format `.pdf`,`.csv`, `.txt`, 
+        Named after the Hitchhiker's Guide to the Galaxy answer to the ultimate question of life,
+        42 is a knowledge discovery engine that utilizes Retrieval Augmented Generation to query uploaded documents in the format `.pdf`,`.csv`, `.txt`, 
         `.jpeg`,`.png` and `.jpg`.
         42 has a chat section where users can chat with the bot without uploading the documents, and a code understanding section where
         you can query a repository on GitHub. For code understanding select the GitHub toggle button and key in the repository url.
