996c03b215fb8de9656026811fa51fb9cef5c334
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-08-01 22:38:46 +0300

    prompt

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -432,7 +432,7 @@ try:
                     assistant_msg = response  # Adjusted to fetch text from the response
 
                     if assistant_msg == "Generated image.":
-                        st.session_state["messages"].append({"role":"assistant","content":f"Here is your generated image:{['IMAGE'](image_url)}, for the description : {user_input}"})
+                        st.session_state["messages"].append({"role":"assistant","content":f"Here is your generated image:{image_url}, for the description : {user_input}"})
                          
 
                     # Append assistant message to session state and display it
