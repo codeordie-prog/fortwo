effa3850936d91e9af6866d7ea462bd787d2532d
Kelvin Ndeti <136579757+codeordie-prog@users.noreply.github.com>
2024-08-05 18:19:28 +0300

    Update fortytwo.py

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -77,7 +77,7 @@ try:
     #Display the image in the center column
     with col1:
         if image_bytes:
-         st.image(io.BytesIO(image_bytes), width=100)
+         st.image(io.BytesIO(image_bytes), width=50)
         else:
          st.error("Failed to load image.")
 
