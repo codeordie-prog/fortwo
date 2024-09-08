b1e7d440a473c4ba3abc59266f97dd08de1a0c77
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-09-08 22:22:27 +0300

    tabs

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -157,16 +157,6 @@ try:
 
 
 
-    #---------------------------------------------------sidebar for query web-------------------------------------------------------------------#
-
-    st.sidebar.subheader("Query web section")
-    st.sidebar.info("""
-                    use this section to interact with web information. 
-                    1. Copy the URL of the webpage and paste it on the URL input section.
-                    2. Enter the document saving name. as you desire.
-                    3. Query the website for information by asking 42 about it.""")
-    
-
     # Inject custom CSS for glowing border effect
     st.markdown(
         """
