916225363d9877b6510079bdd09ad99e07b24947
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-09-10 12:06:42 +0300

    tabs

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -856,7 +856,7 @@ try:
                
              
 
-            if st.sidebar.button("Download chat"):
+            if st.button("Download chat"):
                all_messages = "\n".join([f"{msg['role']}: {msg['content']}" for msg in st.session_state["messages"]])
                create_and_download(all_messages)
 
