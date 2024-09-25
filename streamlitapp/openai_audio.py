dc02ec08d54f0dc3f30b6a91e1f717220b40fed7
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-09-25 23:46:21 +0300

    audio

--- streamlitapp/openai_audio.py
+++ streamlitapp/openai_audio.py
@@ -3,15 +3,24 @@ from openai import OpenAI
 
 
 def text_to_speech(input,api_key):
-    client = OpenAI(api_key=api_key)
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
-        temp_path = temp_file.name  # Get the name of the temporary file
-        response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=input
-        )
-        with open(temp_path,"wb") as f:
-            f.write(response.content)
 
-    return temp_path
+    count = len(input)
+
+    if count<4000:
+
+
+        client = OpenAI(api_key=api_key)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+            temp_path = temp_file.name  # Get the name of the temporary file
+            response = client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=input
+            )
+            with open(temp_path,"wb") as f:
+                f.write(response.content)
+
+        return temp_path
+    
+    else:
+        return None
