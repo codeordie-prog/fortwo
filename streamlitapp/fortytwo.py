f73d246179bca432dcb316dcd00c55b3c68abb28
Kelvin Ndeti <136579757+codeordie-prog@users.noreply.github.com>
2024-10-16 11:41:21 +0300

    Update fortytwo.py

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -150,7 +150,7 @@ try:
             llm_model_chat=st.selectbox(label="choose model",
                                          options=["meta/llama-3.1-405b-instruct","meta/llama-3.2-3b-instruct"])
             
-            include_audio = st.toggle(label="turn on audio responses",key="t2")
+            include_audio = st.toggle(label="turn on audio responses",key="t1")
         
 
     with tab2:
