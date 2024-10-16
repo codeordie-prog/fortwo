50c983bf8d70beddeb434d2ba79c464944314f6e
Kelvin Ndeti <136579757+codeordie-prog@users.noreply.github.com>
2024-10-16 13:09:51 +0300

    Update fortytwo.py

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -550,8 +550,8 @@ try:
                                 #download the audio
                                     
                                 with open(responses_path, "rb") as audio_file:
-                                    data = audio_file.read()
-                                    st.download_button(label="download",data=data,file_name="audio.mp3",mime="audio/mp3")
+                                     data = audio_file.read()
+                                     st.download_button(label="download",data=data,file_name="audio.mp3",mime="audio/mp3")
                                         
                             else:
 
@@ -679,7 +679,7 @@ try:
                                             data = audio_file.read()
                                 
                                         
-                                          st.download_button(label="download",data=data,file_name="audio.mp3",mime="audio/mp3")
+                                            st.download_button(label="download",data=data,file_name="audio.mp3",mime="audio/mp3")
 
 
                                 else:
