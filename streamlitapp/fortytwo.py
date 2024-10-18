f4ef08b482d2d65ed45c7d6459d05f336e18b626
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-10-18 12:54:14 +0300

    o1mini and preview

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -542,8 +542,8 @@ try:
 
                         
 
-                    except Exception:
-                        st.write("an Error occured please enter a valid API key")
+                    except Exception as e:
+                        st.write("an Error occured please enter a valid API key",e)
 
     #---------------------------------------------------------RAG setup section------------------------------------------------------------------#
     #query website function
