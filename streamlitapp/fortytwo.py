6f483ec7cc8c000e5557df7d050c5f38ca9effc2
Kelvin Ndeti <136579757+codeordie-prog@users.noreply.github.com>
2024-10-21 11:24:08 +0300

    Update fortytwo.py

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -470,6 +470,7 @@ try:
                                     nvidia_chain = system_prompt | llm2 | StrOutputParser()
                                     nim_resp = ""
                                     response_display = st.empty()
+                                    st.spinner("Generating response..")
                                     response = nvidia_chain.invoke({"question": user_input,"chat_history":st.session_state["messages"]})
                                     for chunk in response:
                                         nim_resp+=chunk
