548bdb51d38c3189ee015c6a5122cc9127e105e9
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-10-24 12:03:53 +0300

    query docs

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -1055,8 +1055,8 @@ try:
 
 
          
-        except TypeError :
-            st.write("encountered a None type inside main call, check url submitted it might be returning a none type object")
+        except TypeError as e:
+            st.write("encountered a None type inside main call, check url submitted it might be returning a none type object",e)
         except Exception :
              st.write("An error was encountered at main call")
     
