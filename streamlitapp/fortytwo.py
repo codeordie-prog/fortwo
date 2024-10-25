c9837fb309bb4c3d35d59b3e4022537be4d418fe
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-10-25 11:24:10 +0300

    chat docs

--- streamlitapp/fortytwo.py
+++ streamlitapp/fortytwo.py
@@ -661,11 +661,13 @@ try:
 
                                 with st.spinner("`Thinking..`"):
 
-                                    # Determine input source
-                                    query_for_docs = audio_text if audio_text else user_input
+                        
 
                                     if nvidia_api_key: 
                                         if uploaded_chat_documents:
+
+                                            # Determine input source
+                                            query_for_docs = audio_text if audio_text else user_input
             
                                             # Configure retriever and retrieve documents
                                             chat_doc_retriever = configure_retriever(uploaded_files=uploaded_chat_documents)
@@ -685,6 +687,8 @@ try:
                                                 response_display.write(nim_resp)
 
                                         else:
+                                            # Determine input source
+                                            query_for_docs = audio_text if audio_text else user_input
                                             # Run the LLM with a direct query if no documents are uploaded
                                             response = nvidia_chain.invoke({"question": query_for_docs, "chat_history": st.session_state["messages"]})
                                             for chunk in response:
